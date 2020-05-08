542a47a3186e6bd87ca79ef1afc24ed7837b04a9
bartq <bci@poczta.onet.pl>
2020-05-08 16:49:40 +0200

    Hide app bar on mobiles

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useMediaQuery } from 'react-responsive'
+import Dashboard from './components/Dashboard'
 import './App.css';
 
 function App() {
+  const isMobile = useMediaQuery({maxDeviceWidth : 1000})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dashboard isMobile={isMobile} />
     </div>
   );
 }
