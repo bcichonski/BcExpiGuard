b480c4f66e22269e1a4acb1a87cf0e92621d797e
bartq <bci@poczta.onet.pl>
2020-05-08 18:31:21 +0200

    FIX missed router reference

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import { useMediaQuery } from 'react-responsive'
 import Layout from './components/Layout'
-import { BrowserRouter as Router } from "react-router-dom"
 
 function App() {
   const isMobile = useMediaQuery({ maxDeviceWidth: 1000 })
 
   return (
     <div className="App">
-      <Router>
         <Layout isMobile={isMobile} />
-      </Router>
     </div>
   );
 }
